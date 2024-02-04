7a39c54a7709b906d0f8e0005432231f7e8492cb
jos3lo89 <gothangel21@hotmail.com>
2024-02-03 19:34:36 -0500

    edit pc 2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import app from "./app.js";
 import { PORT } from "./config.js";
 import connectedDB from "./db.js";
 
+const appOn = () => {
+  try {
+    app.listen(PORT, () => console.log(`server on por ${PORT}`));
+    connectedDB();
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-app.listen(PORT, () => console.log(`server on por ${PORT}`));
-connectedDB()
+
+appOn()
